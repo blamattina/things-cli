977969d29b7665da487380e222e87f7d54fdc4ac
Brian LaMattina <brianlamattina@gmail.com>
2020-09-26 17:06:26 -0400

    Setup jest

--- test/commands/hello.test.ts
+++ test/commands/hello.test.ts
@@ -1,17 +1,19 @@
-import {expect, test} from '@oclif/test'
-
+import Hello from '../../src/commands/hello'
 describe('hello', () => {
-  test
-  .stdout()
-  .command(['hello'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
+  let result: Array<any>
+
+  beforeEach(() => {
+    result = []
+    jest.spyOn(process.stdout, 'write').mockImplementation((val: any): any => result.push(val))
   })
 
-  test
-  .stdout()
-  .command(['hello', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
+  it('should print hello world', async () => {
+    await Hello.run([])
+    expect(result[0]).toContain('hello world from ./src/commands/hello.ts')
+  })
+
+  it('should print hello blt', async () => {
+    await Hello.run(['--name', 'blt'])
+    expect(result[0]).toContain('hello blt from ./src/commands/hello.ts')
   })
 })
