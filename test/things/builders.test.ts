4ff58bf55d7ec52548e2cb0025639fec47f4546e
Brian LaMattina <brianlamattina@gmail.com>
2020-09-28 23:09:29 -0400

    Add snapshot testing for builders

--- test/things/builders.test.ts
+++ test/things/builders.test.ts
@@ -5,36 +5,19 @@ describe('things/builders', () => {
     it('builds a todo with a title', () => {
       const todoJson = buildTodo({ title: 'test todo' });
 
-      expect(todoJson).toEqual({
-        type: 'to-do',
-        attributes: {
-          title: 'test todo',
-        },
-      });
+      expect(todoJson).toMatchSnapshot();
     });
 
     it('builds a todo with an optional list', () => {
       const todoJson = buildTodo({ title: 'test todo', list: 'test list' });
 
-      expect(todoJson).toEqual({
-        type: 'to-do',
-        attributes: {
-          title: 'test todo',
-          list: 'test list',
-        },
-      });
+      expect(todoJson).toMatchSnapshot();
     });
 
     it('builds a todo with an optional when', () => {
       const todoJson = buildTodo({ title: 'test todo', when: 'today' });
 
-      expect(todoJson).toEqual({
-        type: 'to-do',
-        attributes: {
-          title: 'test todo',
-          when: 'today',
-        },
-      });
+      expect(todoJson).toMatchSnapshot();
     });
   });
 });
